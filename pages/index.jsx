828e27f2a95a1bf4377c618349f0aef117dae55c
seanzjh <41064562+seanzjh@users.noreply.github.com>
2022-08-21 00:37:12 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "iQmMHZpDV9A5WmtDWu1xPNJF5nFpJYz_"
+    const api_key = "dqNT-dvBJ6UGRVg2Tmt5hcnFONMI7Ze3"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
@@ -39,7 +39,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "iQmMHZpDV9A5WmtDWu1xPNJF5nFpJYz_"
+      const api_key = "dqNT-dvBJ6UGRVg2Tmt5hcnFONMI7Ze3"
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
